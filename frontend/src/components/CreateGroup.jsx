5414f01e4d95b61aed3a1955e347478955a95e20
Shashwat Adhikary <147225076+shashwatadhikary@users.noreply.github.com>
2024-12-13 15:56:54 -0500

    Update CreateGroup.jsx

--- frontend/src/components/CreateGroup.jsx
+++ frontend/src/components/CreateGroup.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-axios.defaults.baseURL = 'http://localhost:5001';
+axios.defaults.baseURL = 'https://project-1-m2bn.onrender.com';
 
 
 function CreateGroup() {
