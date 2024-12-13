158e8f1ede2abf66a034bfbc3a8e7345eae5ec46
Shashwat Adhikary <147225076+shashwatadhikary@users.noreply.github.com>
2024-12-13 15:56:20 -0500

    Update Signup.js

--- frontend/src/Signup.js
+++ frontend/src/Signup.js
@@ -19,7 +19,7 @@ function Signup() {
   const handleSignup = async (event) => {
     event.preventDefault();
     try {
-      const response = await fetch('http://localhost:5001/signup', {
+      const response = await fetch('https://project-1-m2bn.onrender.com/signup', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ username, email, password }),
